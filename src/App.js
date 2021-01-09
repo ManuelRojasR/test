71e46a4eeaafd04b9614bcccf291a8900b92f8f7
ManuelRojasR <m.rojas@lifeware.cl>
2021-01-09 12:02:29 -0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto puede ser una página para Lifeware.
         </p>
         <a
           className="App-link"
